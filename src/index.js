1a01deb7d602cf420af462c6f4293ff15a91c03e
kipp <kipphulick@gmail.com>
2022-09-29 00:19:53 -0500

    Base app with redux and reducers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom/client'
+
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { PersistGate } from 'redux-persist/integration/react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import thunk from 'redux-thunk';
+
+import BaseLayout from './components/layout/BaseLayout';
+import Reducer from './components/Reducer';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, Reducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk]
+})
+
+let persistor = persistStore(store)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
+      <Router>
+        <BaseLayout>
+          <Routes>
+            <Route path="/" element={<App />}/>
+            
+          </Routes>
+        </BaseLayout>
+      </Router>
+    </PersistGate>
+    </Provider>
+  </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
